7d98d1c6bf46c3eee3a058560b48dd05ce31a6c3
Nika <ovchinnikovavr@gmail.com>
2026-01-15 18:09:53 +0500

    got info from server

--- src/scripts/components/api.js
+++ src/scripts/components/api.js
@@ -1,1 +1,25 @@
+const config = {
+  baseUrl: "https://mesto.nomoreparties.co/v1/apf-cohort-202",
+  headers: {
+    authorization: "7bb2867b-45c7-424e-be1d-91446cebdf87",
+    "Content-Type": "application/json",
+  },
+};
+
+/* Проверяем, успешно ли выполнен запрос, и отклоняем промис в случае ошибки. */
+const getResponseData = (res) => {
+  return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
+};
+
+export const getUserInfo = () => {
+  return fetch('${config.baseUrl}/users/me', { // Запрос к API-серверу
+    headers: config.headers, // Подставляем заголовки
+  }).then(getResponseData);  // Проверяем успешность выполнения запроса
+};
+
+export const getCardList = () => {
+  return fetch('${config.baseUrl}/cards', { // Запрос к API-серверу
+    headers: config.headers, // Подставляем заголовки
+  }).then(getResponseData);  // Проверяем успешность выполнения запроса
+};
 
