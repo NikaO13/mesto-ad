e44d0e2f05dd23375fe905403681bb61582143eb
NikaO <ovchinnikovavr@gmail.com>
2026-01-18 12:13:28 +0500

    statisctics

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,7 +6,7 @@
   Из index.js не допускается что то экспортировать
 */
 
-import { createCardElement, deleteCard, likeCard } from "./components/card.js";
+import { createCardElement } from "./components/card.js";
 import { openModalWindow, closeModalWindow, setCloseModalWindowEventListeners } from "./components/modal.js";
 import { enableValidation, clearValidation } from "./components/validation.js";
 import { getCardList, getUserInfo, setUserInfo, setAvatarInfo, createNewCard, deleteCardFromServer, changeLike } from "./components/api.js";
@@ -48,6 +48,12 @@ const profileAvatar = document.querySelector(".profile__image");
 const avatarFormModalWindow = document.querySelector(".popup_type_edit-avatar");
 const avatarForm = avatarFormModalWindow.querySelector(".popup__form");
 const avatarInput = avatarForm.querySelector(".popup__input");
+
+const logoOfMesto = document.querySelector('.logo');
+const infoModalWindow = document.querySelector('.popup_type_info');
+const infoList = infoModalWindow.querySelector('.popup__info');
+const popularCardsTitle = infoModalWindow.querySelector('.popup__text');
+const popularCardsList = infoModalWindow.querySelector('.popup__list');
 
 let user_Id = null;
 
@@ -155,12 +161,78 @@ const handlePreviewPicture = ({ name, link }) => {
   openModalWindow(imageModalWindow);
 };
 
+const handleStatistics = () => {
+  getCardList()
+    .then((cards) => {
+      infoList.innerHTML = ''; // очищаем от старого
+      popularCardsList.innerHTML = '';
+      const users = new Set();
+      let totalLikes = 0;
+      const userLikes = {};
+      const cardLikes = [];
+      cards.forEach(card => {
+        users.add(card.owner.name);
+        totalLikes += card.likes.length;
+        card.likes.forEach(user => {
+          userLikes[user.name] = (userLikes[user.name] || 0) + 1; //"||0" - проверка на то, что user`а ещё модет не быть в userLikes
+        });
+        cardLikes.push({
+          name: card.name,
+          likes: card.likes.length
+        })
+      });
+      let championByLikes = '';
+      let maxUserLikes = 0;
+      Object.entries(userLikes).forEach(([userName, likes]) => {
+        if (likes > maxUserLikes) {
+          maxUserLikes = likes;
+          championByLikes = userName;
+        }
+      });
+      cardLikes.sort((a, b) => b.likes - a.likes); //оказывается просто sort в js работает из-под палки
+      const template = document.querySelector('#popup-info-definition-template');
+      const totalUsers = template.content.cloneNode(true);
+      totalUsers.querySelector('.popup__info-term').textContent = 'Всего пользователей:';
+      totalUsers.querySelector('.popup__info-description').textContent = users.size;
+      infoList.appendChild(totalUsers);
+      const sumLikes = template.content.cloneNode(true);
+      sumLikes.querySelector('.popup__info-term').textContent = 'Всего лайков:';
+      sumLikes.querySelector('.popup__info-description').textContent = totalLikes;
+      infoList.appendChild(sumLikes);
+      const maxLikes = template.content.cloneNode(true);
+      maxLikes.querySelector('.popup__info-term').textContent = 'Максимально лайков от одного:';
+      maxLikes.querySelector('.popup__info-description').textContent = maxUserLikes;
+      infoList.appendChild(maxLikes);
+      const champion = template.content.cloneNode(true);
+      champion.querySelector('.popup__info-term').textContent = 'Чемпион лайков:';
+      champion.querySelector('.popup__info-description').textContent = championByLikes;
+      infoList.appendChild(champion);
+      popularCardsTitle.textContent = 'Популярные карточки:';      
+      const cardTemplate = document.querySelector('#popup-info-user-preview-template');
+      cardLikes.slice(0, 3).forEach(card => {
+        const cardItem = cardTemplate.content.cloneNode(true);
+        const listItem = cardItem.querySelector('.popup__list-item');
+        let displayName = card.name;
+        if (displayName.length > 12) {
+          displayName = displayName.substring(0, 12) + '...';
+        }
+        listItem.textContent = displayName;
+        popularCardsList.appendChild(cardItem);
+      });
+      openModalWindow(infoModalWindow);
+    })
+    .catch((err) => {
+      console.log(err);
+  });
+};
+
 enableValidation(validationSettings);
 
 // EventListeners
 profileForm.addEventListener("submit", handleProfileFormSubmit);
 cardForm.addEventListener("submit", handleCardFormSubmit);
 avatarForm.addEventListener("submit", handleAvatarFromSubmit);
+logoOfMesto.addEventListener('click', handleStatistics);
 
 openProfileFormButton.addEventListener("click", () => {
   profileTitleInput.value = profileTitle.textContent;
